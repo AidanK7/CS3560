2e6c503c890854110079b4abd861f492305ce735
AidanK7 <82438468+AidanK7@users.noreply.github.com>
2025-01-07 17:31:10 -0800

    Update student.java

--- student.java
+++ student.java
@@ -60,3 +60,19 @@ public class Student {
         return name.equals(student.name) && major.equals(student.major);
     }
 }
+
+public class StudentTest {
+
+    public static void main(String[] args) {
+
+        Student student1 = new Student("John", "CS", 3.5);
+
+        Student student2 = new Student();
+        student2.setName("Mary Ann");
+        student2.setMajor("CE");
+        student2.setGpa(3.3);
+
+        System.out.println(student1);
+        System.out.println(student2);
+    }
+}
