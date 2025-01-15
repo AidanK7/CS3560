1f5b979c18e213c9eb596e9c189a94f07547c4c7
AidanK7 <82438468+AidanK7@users.noreply.github.com>
2025-01-15 03:30:54 -0800

    Create MainDeleteProduct.java

---
+++ assignment3/question3/b/MainDeleteProduct.java
@@ -0,0 +1,14 @@
+public class MainDeleteProduct {
+
+    public static void main(String[] args) {
+        // Omit session connection
+
+        Product product = session.get(Product.class, 1L); 
+
+        if (product != null) {
+            session.delete(product); 
+        }
+
+        // Omit session closure
+    }
+}
