b05298d60ea66d59274001f4556848f9ae22639c
AidanK7 <82438468+AidanK7@users.noreply.github.com>
2025-01-15 03:25:55 -0800

    Create product.java

---
+++ assignment3/question3/b/product.java
@@ -0,0 +1,16 @@
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="name", nullable = false)
+    private String name;
+
+    @ManyToMany(mappedBy = "products")
+    private List<Order> orders = new ArrayList<>();
+
+}
