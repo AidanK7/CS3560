4abb735c05614560a7128815b9fde6dc1c46394f
AidanK7 <82438468+AidanK7@users.noreply.github.com>
2025-01-15 03:19:09 -0800

    Create MainDelete.java

---
+++ assignment3/question3/a/MainDelete.java
@@ -0,0 +1,19 @@
+public class MainDelete {
+
+    public static void main(String[] args) {
+        // Omiting the session connection statements
+
+        Customer customer = session.get(Customer.class, 1L); 
+
+        if (customer != null) {
+            if (customer.getProfessor() != null) {
+                Professor professor = customer.getProfessor();
+                session.delete(professor);
+            }
+
+            session.delete(customer);
+        }
+
+        // Omit the session closing statements
+    }
+}
