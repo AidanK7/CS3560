606e8e326c5798512527b6a73085aa560c998fc1
AidanK7 <82438468+AidanK7@users.noreply.github.com>
2025-01-15 03:23:55 -0800

    Create order.java

---
+++ assignment3/question3/b/order.java
@@ -0,0 +1,24 @@
+@Entity
+@Table(name = "order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="date", nullable = false)
+    private Date date;
+
+    @Column(name="customerName", nullable = false)
+    private String customerName;
+
+    @ManyToMany
+    @JoinTable(
+        name = "order_detail",
+        joinColumns = @JoinColumn(name = "order_id"),
+        inverseJoinColumns = @JoinColumn(name = "product_id")
+    )
+    private List<Product> products = new ArrayList<>();
+
+}
