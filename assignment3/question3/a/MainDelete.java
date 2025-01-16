ab5f29fcff6ff3535b396c61799a9d790d7ddc33
AidanK7 <82438468+AidanK7@users.noreply.github.com>
2025-01-15 16:08:12 -0800

    Update MainDelete.java

--- assignment3/question3/a/MainDelete.java
+++ assignment3/question3/a/MainDelete.java
@@ -3,7 +3,7 @@ public class MainDelete {
     public static void main(String[] args) {
         // Omiting the session connection statements
 
-        Customer customer = session.get(Customer.class, 1L); 
+        Customer customer = session.get(Customer.class, 1); 
 
         if (customer != null) {
             if (customer.getProfessor() != null) {
