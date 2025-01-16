697af86041a677143a6c9b624c91f3bf2c042a8a
AidanK7 <82438468+AidanK7@users.noreply.github.com>
2025-01-16 15:25:47 -0800

    Update and rename course.java to exam.java

---
+++ exam.java
@@ -0,0 +1,11 @@
+public class Exam {
+
+    private int description;
+    private Course course;
+
+    public Exam(int description, Course course) {
+        this.description = description;
+        this.course = course; 
+    }
+
+}
