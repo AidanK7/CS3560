b63994f792e13d2d69586a7fb8b43593ab47586e
AidanK7 <82438468+AidanK7@users.noreply.github.com>
2025-01-15 16:08:29 -0800

    Update MainDeleteProduct.java

--- assignment3/question3/b/MainDeleteProduct.java
+++ assignment3/question3/b/MainDeleteProduct.java
@@ -3,7 +3,7 @@ public class MainDeleteProduct {
     public static void main(String[] args) {
         // Omit session connection
 
-        Product product = session.get(Product.class, 1L); 
+        Product product = session.get(Product.class, 1); 
 
         if (product != null) {
             session.delete(product); 
