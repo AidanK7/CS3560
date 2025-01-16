d8817e59e7ec6499eab4d0e256dc194f190b59e6
AidanK7 <82438468+AidanK7@users.noreply.github.com>
2025-01-16 15:33:34 -0800

    Update q7-3.java

--- q7-3.java
+++ q7-3.java
@@ -3,9 +3,11 @@ import java.util.Date;
 public class Person {
 
     private int name;
+    private DriverLicense driverLicense;
 
     public Person(int name, DriverLicense driverLicense) {
         this.name = name;
+        this.driverLicense = driverLicense;
     }
 
 }
