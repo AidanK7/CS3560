b8d0852333ef37b634e0ec3a3d2f789df8b74b32
AidanK7 <82438468+AidanK7@users.noreply.github.com>
2025-01-15 21:31:59 -0800

    Update MainCreateProduct.java

--- assignment3/question3/b/MainCreateProduct.java
+++ assignment3/question3/b/MainCreateProduct.java
@@ -2,12 +2,15 @@ public class MainCreateProduct {
 
     public static void main(String[] args) {
         // Omit session connection
+        session.beginTransaction();
 
         Product product = new Product();
         product.setName("Orange Juice");
 
         session.save(product); 
 
+        session.getTransaction().commit();
+        
         // Omit session closure
     }
 }
